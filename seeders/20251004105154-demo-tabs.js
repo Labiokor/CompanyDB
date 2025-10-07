01521c635c149a06c7c1849d016ed14f758c8efd
sampahallen <sampahallen@gmail.com>
2025-10-07 08:21:35 +0000

    Add migration and seeder files for Blogs, Pages, and Tabs

---
+++ seeders/20251004105154-demo-tabs.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, _Sequelize) {
+   await queryInterface.bulkInsert('tabs', [
+    {
+      title: 'Lorem',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, est?',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Lorem',
+      content: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, est?',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]); 
+  },
+
+  async down (queryInterface, _Sequelize) {
+    await queryInterface.bulkDelete('tabs', null, {});
+  }
+};
