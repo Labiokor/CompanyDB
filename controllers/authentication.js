1b19c8fac7045b97263b50a2a3b171396ac63f5e
sampahallen <sampahallen@gmail.com>
2025-10-20 14:13:22 +0000

    fixed the user authentication issue The first issue was sending a response with the token even before the passwords got a chance to be matched. The next thing was that the hashing was being done in the controller and in the db model too. So it double hashed the password. This made it impossible to compare correctly, and that's what me and samuel were facing yesterday. So I took it out of the register side and left the one in the model to do the work. and used the validatePassword method in the models for login validation rather than bcrypt.compare()

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,51 +1,50 @@
-
 const bcrypt = require('bcryptjs');
 const generateToken = require('../generateToken');
 const User = require('../models/users');
 const pass = require('../pass');
 
-
 exports.register = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-    const userExists = await User.findOne({ where: {email} });
-    if (userExists) return res.status(400).json({ message: 'User already exists' });
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists)
+      return res.status(400).json({ message: 'User already exists' });
 
-    const harshedPassword = await bcrypt.hash(password, 10); 
-
-    const user = await User.create({ name, email, password: harshedPassword });
+    const user = await User.create({ name, email, password});
     res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
+      user: {
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (err) {
-     console.error(' Registration error:', err.message);
-  console.error('Stack trace:', err.stack);
-  res.status(500).json({ message: 'Server error' });
-
+    console.error(' Registration error:', err.message);
+    console.error('Stack trace:', err.stack);
+    res.status(500).json({ message: 'Server error' });
   }
 };
 
 exports.login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ where: {email} });
-     if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+    const user = await User.findOne({ where: { email } });
+    if (!user) return res.status(401).json({ message: 'Invalid credentials' });
+    const isMatch = await user.validatePassword(password)
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = generateToken(user.id);
 
-   const token = generateToken(user.id);
-   res.status(200).json({ message: 'Login successful', token });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ message: 'Invalid credentials' });
-
-    // return user info 
-    res.json({
+    // return user info
+    res.status(200).json({
       message: 'Login successful',
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      token
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+      token,
     });
   } catch (err) {
     next(err);
